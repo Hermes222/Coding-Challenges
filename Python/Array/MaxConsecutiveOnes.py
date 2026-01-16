d16081f75a18e60e769440feaa51b7c0fe428a8c
Hermes Chavez <spiderchavez055+github@gmail.com>
2026-01-15 23:15:48 -0500

    max consecutive ones

---
+++ Python/Array/MaxConsecutiveOnes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        left = ans = zero = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero += 1
+            while zero > k:
+                if nums[left]== 0:
+                    zero -= 1
+                left += 1
+            ans = max(ans,right - left + 1)
+        return ans
